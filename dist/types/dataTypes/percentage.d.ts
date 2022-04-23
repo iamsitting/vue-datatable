89b3e763f1ee557bb51b8fef67be62e9dab9b4d8
iamsitting <carlos@iamsitting.com>
2022-04-23 18:08:42 -0500

    include dist (#5)

---
+++ dist/types/dataTypes/percentage.d.ts
@@ -0,0 +1,7 @@
+declare const percentage: {
+    isRight: boolean;
+    format(x: any): any;
+    filterPredicate(rowval: any, filter: string, skipDiacritics?: boolean, fromDropdown?: boolean): boolean;
+    compare(x: any, y: any): 1 | -1 | 0;
+};
+export default percentage;
